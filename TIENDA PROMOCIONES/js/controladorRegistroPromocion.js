42d16983bccacadd823478ea4172c2c5400d8332
DENISO700 <60415650+DENISO700@users.noreply.github.com>
2020-03-16 16:40:08 -0600

    master

--- TIENDA PROMOCIONES/js/controladorRegistroPromocion.js
+++ TIENDA PROMOCIONES/js/controladorRegistroPromocion.js
@@ -75,27 +75,97 @@ if (localStorage.getItem("empresa") == null) {
                 sucursales: [
 
                     {
-                        nombreSucursal: "",
-                        correo: "",
-                        telefono: "",
-                        pais: "",
-                        ciudad: "",
-                        redSocial: "",
-                        url: "",
-                        banner: "",
-                        direccion: "",
-                        latitud: "",
-                        longitud: "",
+                        nombreSucursal: "Variedades Darwin",
+                        correo: "example123@gmail.com",
+                        telefono: "2245-5689",
+                        pais: "Honduras",
+                        ciudad: "San Pedro Sula",
+                        redSocial: "Facebook",
+                        url: "www.facebook.com/VariedadesDarwin",
+                        banner: "/images/all-bg-title.jpg",
+                        direccion: "Col. La Peña",
+                        latitud: "45",
+                        longitud: "54",
+
+                    },
+
+                    {
+                        nombreSucursal: "Variedades Isaac",
+                        correo: "example1243@gmail.com",
+                        telefono: "2245-5656",
+                        pais: "Honduras",
+                        ciudad: "Tegucigalpa",
+                        redSocial: "Facebook",
+                        url: "www.facebook.com/VariedadesIsaac",
+                        banner: "/images/all-bg-title.jpg",
+                        direccion: "Col. La Popular",
+                        latitud: "55",
+                        longitud: "64",
+
+                    },
+
+                    {
+                        nombreSucursal: "Variedades Denis",
+                        correo: "example12345@gmail.com",
+                        telefono: "2245-5646",
+                        pais: "Honduras",
+                        ciudad: "Comayagua",
+                        redSocial: "Facebook",
+                        url: "www.facebook.com/VariedadesDenis",
+                        banner: "/images/all-bg-title.jpg",
+                        direccion: "Col. La Cañada",
+                        latitud: "65",
+                        longitud: "74",
 
                     }
+
                 ],
 
                 productos: [
 
                     {
-                        categoria: "Electrodomesticos",
+                        categoria: "Anime",
                         nombre: "Figura de Anime",
                         precio: "$ 40.00",
+                        cantida: "24",
+                        descripcion: "Lorem ipsum dolor . . . ",
+                        calificacion: 2,
+                        ubicacion: [{
+                            direccion: "Col. La Cañada",
+                            latitud: "25",
+                            longitud: "45",
+                        }],
+                        imagenes: [
+                            "images/gallery-img-12.jpg",
+                            "images/gallery-img-12.jpg",
+                            "images/gallery-img-12.jpg",
+                        ],
+
+                        Comentarios: [
+
+                            {
+                                usuario: "Denis Ordoñez",
+                                calificacion: 3,
+                                contenido: "Lorem ipsum dolor . . . "
+                            },
+                            {
+                                usuario: "Darwin Rodas",
+                                calificacion: 3,
+                                contenido: "Lorem ipsum dolor . . . "
+                            },
+                            {
+                                usuario: "Isaac Ramirez",
+                                calificacion: 4,
+                                contenido: "Lorem ipsum dolor . . . "
+                            }
+
+                        ]
+
+                    },
+                    {
+                        categoria: "Electronicos",
+                        nombre: "Celular",
+                        precio: "$ 400.00",
                         cantida: "24",
                         descripcion: "Lorem ipsum dolor . . . ",
                         calificacion: 2,
@@ -131,6 +201,84 @@ if (localStorage.getItem("empresa") == null) {
                         ]
 
                     },
+                    {
+                        categoria: "Mobiliario",
+                        nombre: "Mesa",
+                        precio: "$ 200.00",
+                        cantida: "24",
+                        descripcion: "Lorem ipsum dolor . . . ",
+                        calificacion: 2,
+                        ubicacion: [{
+                            direccion: "Col. La Cañada",
+                            latitud: "25",
+                            longitud: "45",
+                        }],
+                        imagenes: [
+                            "images/gallery-img-06.jpg",
+                            "images/gallery-img-06.jpg",
+                            "images/gallery-img-06.jpg",
+                        ],
+
+                        Comentarios: [
+
+                            {
+                                usuario: "Denis Ordoñez",
+                                calificacion: 3,
+                                contenido: "Lorem ipsum dolor . . . "
+                            },
+                            {
+                                usuario: "Darwin Rodas",
+                                calificacion: 3,
+                                contenido: "Lorem ipsum dolor . . . "
+                            },
+                            {
+                                usuario: "Isaac Ramirez",
+                                calificacion: 4,
+                                contenido: "Lorem ipsum dolor . . . "
+                            }
+
+                        ]
+
+                    },
+                    {
+                        categoria: "Vestimenta",
+                        nombre: "Chaqueta",
+                        precio: "$ 40.00",
+                        cantida: "24",
+                        descripcion: "Lorem ipsum dolor . . . ",
+                        calificacion: 2,
+                        ubicacion: [{
+                            direccion: "Col. La Cañada",
+                            latitud: "25",
+                            longitud: "45",
+                        }],
+                        imagenes: [
+                            "images/gallery-img-09.jpg",
+                            "images/gallery-img-09.jpg",
+                            "images/gallery-img-09.jpg",
+                        ],
+
+                        Comentarios: [
+
+                            {
+                                usuario: "Denis Ordoñez",
+                                calificacion: 3,
+                                contenido: "Lorem ipsum dolor . . . "
+                            },
+                            {
+                                usuario: "Darwin Rodas",
+                                calificacion: 3,
+                                contenido: "Lorem ipsum dolor . . . "
+                            },
+                            {
+                                usuario: "Isaac Ramirez",
+                                calificacion: 4,
+                                contenido: "Lorem ipsum dolor . . . "
+                            }
+
+                        ]
+
+                    },
                 ],
 
                 promociones: [
@@ -138,11 +286,11 @@ if (localStorage.getItem("empresa") == null) {
                     {
 
                         nombre: "",
-                        precio: "$ 40.00",
+                        precio: "$ 30.00",
                         porcentaje: "",
                         descripcion: "Lorem ipsum dolor . . . ",
-                        inicio: "",
-                        final: "",
+                        inicio: "12/12/2020",
+                        final: "11/11/2021",
 
                         sucursales: [
 
@@ -154,6 +302,7 @@ if (localStorage.getItem("empresa") == null) {
                             }
 
                         ],
+
                         imagenes: [
                             "images/gallery-img-01.jpg",
                             "images/gallery-img-01.jpg",
@@ -222,8 +371,37 @@ cargarProductos();
 function Precio() {
 
     document.getElementById("precioproducto").value = empresas[0].productos[document.getElementById("productos").value].precio;
+
+    document.getElementById("img").innerHTML = `
+
+
+    <div class="col-md-12 ">
+    <img src="${empresas[0].productos[document.getElementById("productos").value].imagenes[0]}" class="w-100 ">
+    </div>
+
+`
+}
+/////////////////////////////////////////////////////////////
+
+//Funcion Cargar Sucursales
+
+function Sucursales() {
+
+    document.getElementById("sucursales").innerHTML = ``;
+
+    for (let s = 0; s < empresas[0].sucursales.length; s++) {
+        document.getElementById("sucursales").innerHTML += `
+    
+    <div>
+      <input id="${empresas[0].sucursales[s].nombreSucursal}"  type="checkbox" value="1">
+      <label for="${empresas[0].sucursales[s].nombreSucursal}" style="background-color: white;">${empresas[0].sucursales[s].nombreSucursal}</label>
+    </div>
+    `;
+
+    }
 }
 
+Sucursales();
 
 
 /////////////////////////////////
